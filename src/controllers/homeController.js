6b5ccd5fe1c2ff6a0175911c88d68198f62a7f1d
viincyy <viincyy126@gmail.com>
2021-10-15 15:06:00 +0700

    Create Usr

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,6 @@
+import db from '../models/index';
+import CRUDService from '../services/CRUDServices';
+
 let getHomepage = (req, res) => {
     return res.render('homepage.ejs');
 };
@@ -6,7 +9,14 @@ let getCRUD = (req, res) => {
     return res.render('crud.ejs');
 };
 
+let postCRUD = async (req, res) => {
+    let msg = await CRUDService.createNewUser(req.body);
+    console.log(msg);
+    return res.send('POST CRUD');
+};
+
 module.exports = {
     getHomepage,
     getCRUD,
+    postCRUD,
 };
