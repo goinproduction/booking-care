f665b1aa01671d88cf05cc67b71e0ddf0eac98f7
viincyy <viincyy126@gmail.com>
2021-10-15 23:05:19 +0700

    Init Client side

---
+++ Server/src/controllers/homeController.js
@@ -0,0 +1,46 @@
+import db from '../models/index';
+import CRUDService from '../services/CRUDServices';
+
+let getHomepage = (req, res) => {
+    return res.render('homepage.ejs');
+};
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+};
+
+let postCRUD = async (req, res) => {
+    let msg = await CRUDService.createNewUser(req.body);
+    console.log(msg);
+    return res.send('POST CRUD');
+};
+
+let displayGetCRUD = async (req, res) => {
+    let response = await CRUDService.getAllUser();
+    res.render('displayCRUD.ejs', { response });
+};
+
+let editCRUD = async (req, res) => {
+    let uid = req.query.id;
+    if (uid) {
+        let udata = CRUDService.getUserInfoById();
+        console.log(udata);
+    }
+};
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+}
+
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    await CRUDService.deleteuserById(id);
+}
+module.exports = {
+    getHomepage,
+    getCRUD,
+    postCRUD,
+    displayGetCRUD,
+    editCRUD,
+};
---
+++ Server/src/route/web.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import homeController from '../controllers/homeController';
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    router.get('/', homeController.getHomepage);
+    router.get('/crud', homeController.getCRUD);
+
+    router.post('/post-crud', homeController.postCRUD);
+    router.get('/get-crud', homeController.displayGetCRUD);
+    router.get('/edit-crud', homeController.editCRUD);
+
+    return app.use('/', router);
+};
+
+module.exports = initWebRoutes;
