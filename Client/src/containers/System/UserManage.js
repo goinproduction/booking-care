2e8e4d9b9d5d724ece6beae0ef7f6bfc075da81b
viincyy <viincyy126@gmail.com>
2021-10-21 01:09:17 +0700

    Fetch data from API

--- Client/src/containers/System/UserManage.js
+++ Client/src/containers/System/UserManage.js
@@ -1,33 +1,74 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUsers } from '../../services/userService';
+
 class UserManage extends Component {
-
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [],
+        };
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if (response && response.success) {
+            this.setState({
+                users: response.users,
+            });
+        }
     }
-
 
     render() {
         return (
-            <div className="text-center">Manage users</div>
+            <div className='users-container'>
+                <div className='title text-center my-5'>
+                    MANAGE USER INFORMATION
+                </div>
+                <div className='users-table mx-5'>
+                    <table id='customers'>
+                        <thead>
+                            <tr>
+                                <th>Email</th>
+                                <th>Firstname</th>
+                                <th>Lastname</th>
+                                <th>Address</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.users.map((user) => (
+                                <tr key={user.id}>
+                                    <td>{user.email}</td>
+                                    <td>{user.firstName}</td>
+                                    <td>{user.lastName}</td>
+                                    <td>{user.address}</td>
+                                    <td>
+                                        <button className='btn-edit'>
+                                            <i class='fas fa-pencil-alt'></i>
+                                        </button>
+                                        <button className='btn-delete'>
+                                            <i class='fas fa-trash'></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         );
     }
-
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+    return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
